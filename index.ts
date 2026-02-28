5a3677c1399d2714beaebd182d2a8d65ab88d38d
gemy3731 <mohamedgamalz3731@gmail.com>
2026-02-28 04:07:11 +0200

    fix cors

--- index.ts
+++ index.ts
@@ -3,28 +3,33 @@ import bootstrap from "./src/app.controller.ts";
 import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
- 
+
 const app = express();
 
 const allowedOrigins = [
   "http://localhost:3001",
   "https://nextjs-social-app-one.vercel.app",
 ];
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
 const startServer = async () => {
   try {
     const port = process.env.PORT || 3000;
-    app.use(
-      cors({
-        origin: function (origin, callback) {
-          if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-          } else {
-            callback(new Error("Not allowed by CORS"));
-          }
-        },
-        credentials: true,
-      })
-    );
+
+    app.use(cors(corsOptions));
+    app.options("*", cors(corsOptions));
+    
     await bootstrap(express, app);
     app.listen(port, () => {
       console.log("Server is running on port 3000");
