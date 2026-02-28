197ace12266abf645673a9ac80199e92f0a8c994
gemy3731 <mohamedgamalz3731@gmail.com>
2026-02-28 03:59:12 +0200

    fix(auth): fix cookies

--- src/modules/authModule/auth.controller.ts
+++ src/modules/authModule/auth.controller.ts
@@ -49,7 +49,7 @@ export const logout = (req: Request, res: Response) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production"?"none":"strict",
   });
   res.status(200).json({ message: "Logged out successfully" });
 };
